a5143e668910d51a4ee7d7a487d7ae122095e089
botiurxonmuydinov-ctrl <botiurxonmuydinov@gmail.com>
2026-07-18 01:23:57 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,27 @@
+<?php
 
+// =========================
+// Telegram Bot Sozlamalari
+// =========================
+
+// BotFather bergan tokenni shu yerga yozing
+define('BOT_TOKEN', '8717274081:AAHUsuiDakcQvHi5sPKDaFB7DH9OKKuuHAU');
+
+// Telegram API manzili
+define('API_URL', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
+
+// =========================
+// MySQL Sozlamalari
+// =========================
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'korean_bot');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// =========================
+// Admin
+// =========================
+
+// Admin Telegram ID
+define('ADMIN_ID', 6938970321);
