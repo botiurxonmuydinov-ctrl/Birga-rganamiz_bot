677368ffb8136b4e96236ccc75fc246c8b5942c9
botiurxonmuydinov-ctrl <botiurxonmuydinov@gmail.com>
2026-07-18 01:40:35 +0500

    Update database.php

--- database.php
+++ database.php
@@ -3,17 +3,14 @@
 require_once "config.php";
 
 try {
-
-    $db = new PDO(
+    $pdo = new PDO(
         "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4",
         DB_USER,
         DB_PASS
     );
 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
-
     die("Database ulanmadi: " . $e->getMessage());
-
 }
