2b213d83d3d8059b10124ad958d525d246e4238e
botiurxonmuydinov-ctrl <botiurxonmuydinov@gmail.com>
2026-07-18 01:31:41 +0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,1 +1,14 @@
+<?php
 
+require_once "config.php";
+
+// Telegramdan kelgan ma'lumot
+$update = json_decode(file_get_contents("php://input"), true);
+
+// Agar xabar kelmagan bo'lsa
+if (!$update) {
+    exit;
+}
+
+// index.php ga uzatish
+require_once "index.php";
